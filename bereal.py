06b0f3e42a6f56a44e52e2b603de86854e3e4728
Adheesh Wadkar <apwadkar@pm.me>
2023-02-23 00:57:39 -0500

    Fixed dates, degen times, auto-closes thread

--- bereal.py
+++ bereal.py
@@ -7,23 +7,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MST = pytz.timezone('America/Phoenix')
+
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix=".", intents=intents)
+bot.thread = None
+bot.count = 0
 
-
-bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
-
-global dt
-global dateformat
-dt = datetime.datetime.now()
-dateformat = dt.strftime('%x')
-
+message = "<@&1068375452885987370> :warning: It's time to be fucking for real! You have 10 minutes to DM me 2 images :warning: DM me `.submit` with 2 images. The first image is the front camera, the second is the back."
 
 async def createThread():
-    
-
+    dt = datetime.datetime.now()
+    dateformat = dt.strftime('%x')
+    if bot.thread:
+        await bot.thread.edit(archived=True, locked=True)
     channel = bot.get_channel(1068374707218427966)
-    global thread
     thread = await channel.create_thread(
         name= (dateformat),
         type= discord.ChannelType.private_thread,
@@ -33,16 +31,12 @@ async def createThread():
     embed.set_author(name="BeFuckingForRealBot", icon_url="https://upload.wikimedia.org/wikipedia/en/4/40/BeReal_logo.png")
     embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/en/4/40/BeReal_logo.png")
     await thread.send(embed=embed)
-    return thread
+    bot.thread = thread
 
-#when this command is disabled it gives you an error message when someone uses it
-# and idk how to change that, the error msg doesnt rlly mean anythin tho 
-#cuz i want it to be disabled i tried doing it with the error() function but it doesnt work.
 @bot.command()
 async def submit(ctx):
-
     member = ctx.author
-
+    thread = ctx.bot.thread
      
     if len(ctx.message.attachments) == 2:
         await thread.send("{} Was Being Fucking For Real Today!".format(ctx.message.author.mention))
@@ -51,69 +45,55 @@ async def submit(ctx):
                 
         embed = discord.Embed(title = "{}'s BeReal".format(name), color=discord.Color.random(), timestamp=ctx.message.created_at)
         embed.set_author(name=f"{name}", icon_url=f"{pfp}")
-                
+
         url1 = ctx.message.attachments[0].url
         embed.set_thumbnail(url=(url1))
         url2 = ctx.message.attachments[1].url
         embed.set_image(url=(url2))
         await thread.send(embed=embed)
+        bot.count += 1
     else:
         await ctx.send("Please submit 2 images!!!")
+
+async def BeRealNotif():
+    while True:
+        now = datetime.datetime.now(MST)
+
+        h = random.randint(8, 23)
+        m  = random.randint (0, 59)
+        s = random.randint(0, 59)
+        print(f'{h} {m} {s}')
+
+        then = now.replace(hour=h, minute = m, second= s) + datetime.timedelta(days=1)
+        print(f'{then!r}')
+
+        await asyncio.sleep((then-now).total_seconds())
+
+        await notify()
+
+async def notify(current_thread):
+    channel = bot.get_channel(1068374707218427966)
+    msg = await channel.send(message)
+
+    command.update(enabled=True)
+    await createThread()
+    wait2_time = datetime.timedelta(minutes=10).total_seconds()
+    await asyncio.sleep(wait2_time)
+    command.update(enabled=False)
+    await msg.edit(content=f"Time's up losers. Number of people that were fucking real today: {bot.count}")
+
+@bot.command()
+async def bereal(ctx):
+    await notify(None)
+    await BeRealNotif()
 
 global command
 command = bot.get_command('submit')
 command.update(enabled=False)
 
-async def BeRealNotif():
-    while True:
-        now = datetime.datetime.now(MST)
-        print(now)
-
-        h = random.randint(0, 23)
-        m  = random.randint (0, 59)
-        s = random.randint(0, 59)
-
-        then = now.replace(hour=h, minute = m, second= s)
-        then += datetime.timedelta(days=1)
-        print(h)
-        print(m)
-        print(s)
-
-        wait_time=(then-now).total_seconds()
-        await asyncio.sleep(wait_time)
-
-        
-        channel = bot.get_channel(1068374707218427966)
-        role = "<@&1068375452885987370>"
-
-        await channel.send(role + " :warning: It's time to be fucking for real! You have 10 minutes to DM me 2 images :warning: DM me `.submit` with 2 images. The first image is the front camera, the second is the back.")
-        await itsTime()
-
-@bot.command()
-async def bereal(ctx):
-
-    channel = bot.get_channel(1068374707218427966)
-    role = "<@&1068375452885987370>"
-
-    await channel.send(role + " :warning: It's time to be fucking for real! You have 3 minutes to DM me 2 images :warning: ")
-    await itsTime()
-
-    await BeRealNotif()
-
-async def itsTime():
-    timenow = datetime.datetime.now(MST)
-    timelimit = timenow + datetime.timedelta(minutes=10)
-    command.update(enabled=True)
-    await createThread()
-    wait2_time = (timelimit-timenow).total_seconds()
-    await asyncio.sleep(wait2_time)
-    command.update(enabled=False)
-
-
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
-   # error()
+    print(f'We have logged in as {bot.user}')
 
 # u gotta replace this with ur botty bot key or sum
 bot.run(os.getenv('DISCORD_TOKEN', default=''))
